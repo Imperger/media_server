1f029f4e70de4393458685bfc0643ec2f7445a58
Imperger <mr.imperger@hotmail.com>
2024-09-16 14:21:41 +0300

    feat: Bracket icons

---
+++ client/src/lib/components/icons/close-bracket-icon.tsx
@@ -0,0 +1,42 @@
+import { SvgIcon, SvgIconProps } from '@mui/material';
+
+export default function CloseBracketIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props}>
+      <svg
+        width="512"
+        height="512"
+        viewBox="0 0 135.46667 135.46667"
+        version="1.1"
+        id="svg1"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+      >
+        <rect
+          id="rect1"
+          width="16.933334"
+          height="118.53334"
+          x="-84.666664"
+          y="-127"
+          transform="scale(-1)"
+        />
+        <rect
+          id="rect2"
+          width="25.4"
+          height="16.933336"
+          x="-67.733337"
+          y="-127"
+          transform="scale(-1)"
+        />
+        <rect
+          id="rect3"
+          width="25.4"
+          height="16.933329"
+          x="-67.73333"
+          y="-25.399994"
+          transform="scale(-1)"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
---
+++ client/src/lib/components/icons/open-bracket-icon.tsx
@@ -0,0 +1,39 @@
+import { SvgIcon, SvgIconProps } from '@mui/material';
+
+export default function OpenBracketIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props}>
+      <svg
+        width="512"
+        height="512"
+        viewBox="0 0 135.46667 135.46667"
+        version="1.1"
+        id="svg1"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+      >
+        <rect
+          id="rect1"
+          width="16.933334"
+          height="118.53334"
+          x="42.333336"
+          y="8.4666624"
+        />
+        <rect
+          id="rect2"
+          width="25.4"
+          height="16.933336"
+          x="59.266666"
+          y="8.4666662"
+        />
+        <rect
+          id="rect3"
+          width="25.4"
+          height="16.933329"
+          x="59.26667"
+          y="110.06667"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
