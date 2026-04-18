fb4b346eaa8fe1404436b3c9d7f31cade05fc249
Imperger <mr.imperger@hotmail.com>
2026-04-18 18:11:22 +0300

    feat: scroll restoration after using apps

--- client/src/collection/open-with-file-dialog.tsx
+++ client/src/collection/open-with-file-dialog.tsx
@@ -12,8 +12,12 @@ import {
   ListItemIcon,
   ListItemText
 } from '@mui/material';
+import { useMemo } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
+import { updateLastWatched } from './store/last-watched';
+
+import { useAppDispatch } from '@/hooks';
 import { RWState } from '@/lib/rw-state';
 
 export interface OpenWithFileDialogProps extends RWState<'open', boolean> {
@@ -29,6 +33,11 @@ export default function OpenWithFileDialog({
 }: OpenWithFileDialogProps) {
   const baseURL = import.meta.env.BASE_URL;
   const onClose = () => setOpen(false);
+  const dispatch = useAppDispatch();
+
+  const name = useMemo(() => filename.split('/').slice(-1)[0], [filename]);
+
+  const saveLastWacthed = () => dispatch(updateLastWatched(name));
 
   return (
     <Dialog open={open} onClose={onClose}>
@@ -39,6 +48,7 @@ export default function OpenWithFileDialog({
             <ListItemButton
               component={RouterLink}
               to={`${baseURL}app/clip`}
+              onClick={saveLastWacthed}
               state={{ filename, assetPrefix }}
             >
               <ListItemIcon>
@@ -51,6 +61,7 @@ export default function OpenWithFileDialog({
             <ListItemButton
               component={RouterLink}
               to={`${baseURL}app/tag`}
+              onClick={saveLastWacthed}
               state={{ mode: 'file', filename, assetPrefix }}
             >
               <ListItemIcon>
